876e915f0cdb34e94ec37c2fd5a4cbc87755bcd5
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-27 17:49:31 -0400

    Add commands for SELECT from Nobel Tutorial

--- commands.sql
+++ commands.sql
@@ -81,3 +81,80 @@ AND name LIKE '%i%'
 AND name LIKE '%o%'
 AND name LIKE '%u%'
 AND name NOT LIKE '% %';
+
+-- SELECT from Noble Tutorial
+
+-- Winners from 1950
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1950;
+
+-- 1962 Literature
+SELECT winner
+FROM nobel
+WHERE yr = 1962
+AND subject = 'Literature';
+
+-- Albert Einstein
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
+
+-- Recent Peace Prizes
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000;
+
+-- Literature in 1980's
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+
+-- Only Presidents
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama');
+
+-- John
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John %';
+
+-- Chemistry and Physics from different years
+SELECT yr, subject, winner
+FROM nobel
+WHERE (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'Chemistry');
+
+-- Exclude Chemists and Medics
+SELECT yr, subject, winner
+FROM nobel
+WHERE yr = 1980 AND subject != 'Chemistry' AND subject != 'Medicine';
+
+-- Early Medicine, Late Literature
+SELECT yr, subject, winner
+FROM nobel
+WHERE (subject = 'Medicine' AND yr < 1910) OR (subject = 'Literature' AND yr >= 2004);
+
+-- Umlaut
+SELECT *
+FROM nobel
+WHERE winner = 'Peter Grünberg';
+
+-- Apostrophe
+SELECT *
+FROM nobel
+WHERE winner = 'Eugene O''Neill';
+
+-- Knights of the realm
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner;
+
+-- Chemistry and Physics last
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
