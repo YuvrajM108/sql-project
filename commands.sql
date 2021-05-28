8011b39a423895c33710542069e4ff050da107e6
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-28 16:00:28 -0400

    Add commands for The JOIN operation

--- commands.sql
+++ commands.sql
@@ -268,3 +268,71 @@ SELECT continent
 FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000;
+
+-- The JOIN operation
+
+-- 1
+SELECT matchid, player FROM goal 
+WHERE teamid = 'GER';
+
+-- 2
+SELECT id,stadium,team1,team2
+FROM game
+WHERE id = 1012;
+
+-- 3
+SELECT goal.player, goal.teamid, game.stadium, game.mdate
+FROM game JOIN goal ON (game.id=goal.matchid)
+WHERE goal.teamid = 'GER';
+
+-- 4
+SELECT game.team1, game.team2, goal.player
+FROM game JOIN goal ON (game.id=goal.matchid)
+WHERE goal.player LIKE 'Mario%';
+
+-- 5
+SELECT goal.player, goal.teamid, eteam.coach, goal.gtime
+FROM goal JOIN eteam ON goal.teamid=eteam.id
+WHERE goal.gtime<=10;
+
+-- 6
+SELECT game.mdate, eteam.teamname
+FROM game JOIN eteam ON (game.team1=eteam.id)
+WHERE eteam.coach = 'Fernando Santos';
+
+-- 7
+SELECT goal.player
+FROM goal JOIN game ON goal.matchid=game.id
+WHERE game.stadium = 'National Stadium, Warsaw';
+
+-- 8
+SELECT DISTINCT goal.player
+FROM goal JOIN game ON goal.matchid = game.id 
+WHERE goal.teamid != 'GER' AND (game.team1='GER' OR game.team2='GER');
+
+-- 10
+SELECT game.stadium, COUNT(goal.matchid)
+FROM game JOIN goal ON game.id = goal.matchid
+GROUP BY game.stadium;
+
+-- 11
+SELECT goal.matchid, game.mdate, COUNT(goal.matchid)
+FROM game JOIN goal ON goal.matchid = game.id 
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY goal.matchid, game.mdate;
+
+-- 12
+SELECT goal.matchid, game.mdate, COUNT(goal.matchid)
+FROM game JOIN goal ON goal.matchid = game.id
+WHERE goal.teamid = 'GER'
+GROUP BY goal.matchid, game.mdate;
+
+-- 13
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM (CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id
+  GROUP BY mdate, matchid, team1, team2
+  ORDER BY mdate, matchid, team1, team2;
