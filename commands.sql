a885a9b2889734685f5b4920d674745d4daf7379
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-28 22:38:59 -0400

    Add commands for Using Null

--- commands.sql
+++ commands.sql
@@ -427,3 +427,60 @@ SELECT name
 FROM actor JOIN casting ON actor.id=casting.actorid
 WHERE movieid IN (SELECT movieid FROM casting JOIN actor ON actor.id = casting.actorid 
 AND actor.name = 'Art Garfunkel') AND name!='Art Garfunkel';
+
+-- Using Null
+
+-- 1
+SELECT name
+FROM teacher
+WHERE dept IS NULL;
+
+-- 2
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 3
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 4
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 5
+SELECT name, COALESCE(mobile, '07986 444 2266')
+FROM teacher;
+
+-- 6
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+-- 7
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
+
+-- 8
+SELECT dept.name, COUNT(teacher.id)
+FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id)
+GROUP BY dept.name;
+
+-- 9
+SELECT teacher.name, CASE WHEN (dept.id=1 OR dept.id=2)
+                          THEN 'Sci'
+                          WHEN (dept.id!=1 AND dept.id!=2) OR dept.id IS NULL
+                          THEN 'Art'
+                       END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
+
+-- 10
+SELECT teacher.name, CASE WHEN (dept.id=1 OR dept.id=2)
+                          THEN 'Sci'
+                          WHEN dept=3
+                          THEN 'Art'
+                    WHEN (dept.id!=1 AND dept.id!=2 AND dept.id!=3) OR dept.id IS NULL
+                          THEN 'None'
+                     END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
